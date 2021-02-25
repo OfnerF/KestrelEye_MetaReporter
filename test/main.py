5415aa429227052df9e636de3a0d376990cc6775
Ofner, Fabian <fabianofner@gmx.at>
2021-02-25 17:19:03 +0100

    works with different kind of data

--- test/main.py
+++ test/main.py
@@ -9,12 +9,23 @@ result_path_2 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\session11\\S11
 model_path_3 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\session11\\S11_LNRN18s_new_nt_bw4_Adam"
 result_path_3 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\session11\\S11_LNRN18s_new_nt_bw4_Adam"
 
-session_path = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\session11"
+session_path_1 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\session11"
 
 config_path = "D:\\Karriere\\KestrelEye\\MetaReporter\\src\\config\\config.json"
 
-meta_reporter_1 = MetaReporter(model_path_1, result_path_1, config_path=config_path, level=0)
-print(meta_reporter_1)
+model_path_4 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\image_classification\\config1"
+
+model_path_5 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\image_classification\\config2"
+
+session_path_2 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\image_classification"
+
+model_path_6 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\image_rating\\config1"
+model_path_7 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\image_rating\\config2"
+
+session_path_3 = "D:\\Karriere\\KestrelEye\\MetaReporter\\reports\\image_rating"
+
+# meta_reporter_1 = MetaReporter(model_path_1, result_path_1, config_path=config_path, level=0)
+# print(meta_reporter_1)
 
 # meta_reporter_2 = MetaReporter(model_path_2, result_path_2, config_path=config_path, level=0)
 # print(meta_reporter_2)
@@ -22,5 +33,23 @@ print(meta_reporter_1)
 # meta_reporter_3 = MetaReporter(model_path_3, result_path_3, config_path=config_path, level=0)
 # print(meta_reporter_3)
 
-# meta_reporter_session = MetaReporter(session_path, session_path, config_path, level=1)
-# print(meta_reporter_session)
+# meta_reporter_session_1 = MetaReporter(session_path_1, session_path_1, config_path, level=1)
+# print(meta_reporter_session_1)
+
+# meta_reporter_4 = MetaReporter(model_path_4, model_path_4, config_path=config_path, level=0)
+# print(meta_reporter_4)
+
+# meta_reporter_5 = MetaReporter(model_path_5, model_path_5, config_path=config_path, level=0)
+# print(meta_reporter_5)
+
+# meta_reporter_session_2 = MetaReporter(session_path_2, session_path_2, config_path=config_path, level=1)
+# print(meta_reporter_session_2)
+
+# meta_reporter_6 = MetaReporter(model_path_6, model_path_6, config_path=config_path, level=0)
+# print(meta_reporter_6)
+
+# meta_reporter_7 = MetaReporter(model_path_7, model_path_7, config_path=config_path, level=0)
+# print(meta_reporter_7)
+
+#meta_reporter_session_3 = MetaReporter(session_path_3, session_path_3, config_path=config_path, level=1)
+#print(meta_reporter_session_3)
